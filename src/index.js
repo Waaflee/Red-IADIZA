00a7d37011dad326a90643f314c67f13f5a8d026
Waaflee <agustinwafle@gmail.com>
2018-05-21 17:16:31 -0300

    Processed build for github pages publishing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-// import 'animate.css';
-// import './css/main.scss';
 import main from './js/main';
-// import Glide from '@glidejs/glide';
 import 'aos/dist/aos.css';
 import AOS from 'aos/dist/aos';
 
 AOS.init({
-  offset: 225,
+  offset: 120,
   duration: 500,
   easing: 'ease-in-sine',
   delay: 100,
